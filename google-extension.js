410c1c712242c7c563f4e5041e0890ae632dae70
Bret Hansen <bret.hansen@ellucian.com>
2021-11-04 09:04:42 -0700

    Default publisher to blank
    Developers need to use an appropriate publisher name of their own

--- google-extension.js
+++ google-extension.js
@@ -1,26 +1,26 @@
 module.exports = {
-    'name': 'Google Productivity Tools',
-    'publisher': 'Ellucian',
-    "configuration": {
-        "client": [{
-                "key": "googleOAuthClientId",
-                "label": "Google OAuth Client ID",
-                "type": "string",
-                "required": true
+    name: 'Google Productivity Tools',
+    publisher: '',
+    configuration: {
+        client: [{
+            key: 'googleOAuthClientId',
+            label: 'Google OAuth Client ID',
+            type: 'string',
+            required: true
             }]
     },
-    'cards': [{
-        'type': 'DriveCard',
-        'source': './src/google/cards/GoogleDriveCard',
-        'title': 'Google Drive',
-        'displayCardType': 'Google Drive',
-        'description': 'Google Drive card'
+    cards: [{
+        type: 'DriveCard',
+        source: './src/google/cards/GoogleDriveCard',
+        title: 'Google Drive',
+        displayCardType: 'Google Drive',
+        description: 'Google Drive card'
     },
     {
-        'type': 'MailCard',
-        'source': './src/google/cards/GMailCard',
-        'title': 'Gmail',
-        'displayCardType': 'Gmail',
-        'description': 'Google Gmail card'
+        type: 'MailCard',
+        source: './src/google/cards/GMailCard',
+        title: 'Gmail',
+        displayCardType: 'Gmail',
+        description: 'Google Gmail card'
     }]
 }
--- microsoft-extension.js
+++ microsoft-extension.js
@@ -1,36 +1,36 @@
 module.exports = {
-    "name": "Microsoft Productivity Tools",
-    "publisher": "Ellucian",
-    "configuration": {
-        "client": [{
-            "key": "aadRedirectUrl",
-            "label": "Azure AD Redirect URL",
-            "type": "url",
-            "required": true
+    name: 'Microsoft Productivity Tools',
+    publisher: '',
+    configuration: {
+        client: [{
+            key: 'aadRedirectUrl',
+            label: 'Azure AD Redirect URL',
+            type: 'url',
+            required: true
         }, {
-            "key": "aadClientId",
-            "label": "Azure AD Application (Client) ID",
-            "type": "text",
-            "required": true
+            key: 'aadClientId',
+            label: 'Azure AD Application (Client) ID',
+            type: 'text',
+            required: true
         }, {
-            "key": "aadTenantId",
-            "label": "Azure AD Tenant ID",
-            "type": "text",
-            "required": true
+            key: 'aadTenantId',
+            label: 'Azure AD Tenant ID',
+            type: 'text',
+            required: true
         }]
 	},
-    "cards": [{
-        "type": "OutlookCard",
-        "source": "./src/microsoft/cards/OutlookMailCard",
-        "title": "Outlook",
-        "displayCardType": "Outlook",
-        "description": "This card displays Office 365 Mail"
+    cards: [{
+        type: 'OutlookCard',
+        source: './src/microsoft/cards/OutlookMailCard',
+        title: 'Outlook',
+        displayCardType: 'Outlook',
+        description: 'This card displays Office 365 Mail'
     },
     {
-        "type": "OneDriveCard",
-        "source": "./src/microsoft/cards/OneDriveCard",
-        "title": "OneDrive",
-        "displayCardType": "OneDrive",
-        "description": "This card displays OneDrive"
+        type: 'OneDriveCard',
+        source: './src/microsoft/cards/OneDriveCard',
+        title: 'OneDrive',
+        displayCardType: 'OneDrive',
+        description: 'This card displays OneDrive'
     }]
 }
