6fe9de8afafeddff3969d8da3447b0ea71160a40
Lucas Shaw <73844467+LucasRShaw@users.noreply.github.com>
2022-01-10 13:36:55 -0700

    Add files via upload

---
+++ src/components/w75_NoEmail.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { IMAGES } from '@ellucian/react-design-system/core';
+
+import { useComponents, useIntl } from '../context-hooks/card-context-hooks';
+
+import FullCardLinkedMessage from './w75_FullCardLinkedMessage';
+
+function NoEmails() {
+    const { intl } = useIntl();
+    const { noEmail } = useComponents();
+
+    if (!noEmail) {
+        return null;
+    }
+
+    return (
+        <FullCardLinkedMessage
+            imageName={IMAGES.NO_TASKS}
+            title={intl.formatMessage({id: noEmail.titleId})}
+            message={intl.formatMessage({id: noEmail.messageId})}
+            url='https://outlook.com/nwc.edu'
+        />
+    );
+}
+
+export default NoEmails;
